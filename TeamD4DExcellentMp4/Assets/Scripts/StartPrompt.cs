c0f7237fe5f99dadc101bcd4607b7b79cf4e0524
Connor McGwire <cdmcgwire@live.com>
2016-04-28 11:43:05 -0700

    Final Changes before submission

---
+++ TeamD4DExcellentMp4/Assets/Scripts/StartPrompt.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPrompt : MonoBehaviour {
+	public string inputKey = "Jump";
+	void Update () {
+		if (Input.GetButtonDown("Jump"))
+			gameObject.SetActive(false);
+	}
+}
